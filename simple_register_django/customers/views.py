4f64fc54f1c1d3d69e86a5a602d100c5baba6263
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-06-01 15:04:00 +0700

    :sparkles: Support save data when submit form

--- simple_register_django/customers/views.py
+++ simple_register_django/customers/views.py
@@ -1,7 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+
+from .models import Customer
 
 
 class LandingPageView(View):
     def get(self, request):
         return render(request, 'index.html')
+    
+    def post(self, request):
+        email = request.POST['email']
+        Customer.objects.create(email=email)
+
+        return HttpResponse()
