b86f65cbacb3c302648671e317ac5f44bd9d3a37
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-06-01 13:31:53 +0700

    :recycle: :truck: Split test and moving to tests directory

--- simple_register_django/customers/tests/test_admin.py
+++ simple_register_django/customers/tests/test_admin.py
@@ -1,15 +1,8 @@
 from django.test import TestCase
 from django.contrib import admin
 
-from .admin import CustomerAdmin
-from .models import Customer
-
-
-class CustomerTest(TestCase):
-    def test_customer_model_should_have_email_field(self):
-        expected = 'wow@doge.wow'
-        customer = Customer.objects.create(email='wow@doge.wow')
-        self.assertEqual(customer.email, expected)
+from ..admin import CustomerAdmin
+from ..models import Customer
 
 
 class CustomerAdminTest(TestCase):
