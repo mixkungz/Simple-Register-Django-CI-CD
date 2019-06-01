e3beb467275150d18ecbad0bd56b028118827fc3
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-06-01 11:24:46 +0700

    :tada: Initialize customer app with test and model

---
+++ simple_register_django/customers/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from .models import Customer
+
+
+class CustomerTest(TestCase):
+    def test_customer_model_should_have_email_field(self):
+        expected = 'wow@doge.wow'
+        customer = Customer.objects.create(email='wow@doge.wow')
+        self.assertEqual(customer.email, expected)
