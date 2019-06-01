3b6745ee9076e34f39a8337d20490162761090d3
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-06-01 14:26:56 +0700

    Add landing page view

--- simple_register_django/customers/views.py
+++ simple_register_django/customers/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        html = '<form action="." method="post"><input type="email" name="email"><button type="submit">Submit</button></form>'
+        return HttpResponse(html)
