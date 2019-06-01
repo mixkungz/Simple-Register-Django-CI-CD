05d1aa446603762eec0722301da0a27b8b05575f
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-06-01 14:29:22 +0700

    :recycle: Split html use template html

--- simple_register_django/customers/views.py
+++ simple_register_django/customers/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 
 class LandingPageView(View):
     def get(self, request):
-        html = '<form action="." method="post"><input type="email" name="email"><button type="submit">Submit</button></form>'
-        return HttpResponse(html)
+        return render(request, 'index.html')
