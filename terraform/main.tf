43b1c2b2f8d1a7c8a2c408c3880f76768b706ff6
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-06-02 09:55:57 +0700

    :wrench: Add create instance config

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,26 @@ provider "google" {
   region      = "asia-southeast1"
   zone        = "asia-southeast1-b"
 }
+
+resource "google_compute_instance" "tf_instance" {
+  name          = "tf-instance"
+  machine_type  = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      image     = "ubuntu-1804-bionic-v20190530"
+      size      = 30
+    }
+  }
+
+  metadata = {
+    sshKeys = "pcr.mixkungz:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["my-webs"]
+}
